d01b38a0c32639db41a4ee57cfacb9270c0ea1d3
Arsalan Khattak <me@khattak.dev>
2025-04-21 13:58:44 +0500

    cleanup

--- models/Assignment.ts
+++ models/Assignment.ts
@@ -7,47 +7,6 @@ class AssignmentModel extends Model {
   declare resourceId: number;
 }
 
-// Assignment.init(
-//   {
-//     id: {
-//       type: DataTypes.INTEGER,
-//       primaryKey: true,
-//       autoIncrement: true,
-//     },
-//     eventId: {
-//       type: DataTypes.INTEGER,
-//       allowNull: false,
-//       references: {
-//         model: "events",
-//         key: "id",
-//       },
-//       onDelete: "CASCADE", // Ensures that deleting an 'event' will delete related 'assignments'
-//     },
-//     resourceId: {
-//       type: DataTypes.INTEGER,
-//       allowNull: false,
-//       references: {
-//         model: "resources",
-//         key: "id",
-//       },
-//       onDelete: "CASCADE", // This will delete all assignments referencing the resource when it's deleted
-//     },
-//   },
-//   {
-//     tableName: "assignments",
-//     sequelize,
-//     timestamps: false,
-//     indexes: [
-//       {
-//         fields: ["eventId"],
-//       },
-//       {
-//         fields: ["resourceId"],
-//       },
-//     ],
-//   }
-// )
-
 
 const Assignment = sequelize.define<AssignmentModel>(
   "Assignment",
